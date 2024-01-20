ef38062059a3c44e74ae2e350b1af9d7bd906ef1
WambiruL <wambirulorraine@gmail.com>
2024-01-20 11:42:12 +0300

    Add html templates and add the views to show the messages sent and received

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+def chatbot(request):
+    if request.method=='POST':
+        message=request.POST.get('message')
+        response="Hi this is my response"
+        return JsonResponse({'message':message, 'response':response})
+    return render(request, 'chatbot.html') #to show which file to render in the frontend
