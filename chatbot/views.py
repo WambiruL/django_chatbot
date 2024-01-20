60b3cabf3e027de08bfe9dca15f4806678686e51
WambiruL <wambirulorraine@gmail.com>
2024-01-20 12:22:26 +0300

    Connect to openai

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,10 +1,26 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+import openai
+
+openai_api_key='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'
+openai.api_key=openai_api_key
+
+def ask_openai(message):
+    response=openai.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=message,
+        max_tokens=150,
+        stop=None,
+        temperature=0.7,
+    )
+    print(response)
+    answer=response.choice[0].text.strip()
+    return answer
 
 # Create your views here.
 def chatbot(request):
     if request.method=='POST':
         message=request.POST.get('message')
-        response="Hi this is my response"
+        response= ask_openai(message)
         return JsonResponse({'message':message, 'response':response})
     return render(request, 'chatbot.html') #to show which file to render in the frontend
