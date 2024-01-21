954b8c6d0f2c2800ba475fe727f9cca52f1b8f8f
WambiruL <wambirulorraine@gmail.com>
2024-01-21 11:16:58 +0300

    Add the login and logout views

--- chatbot/views.py
+++ chatbot/views.py
@@ -28,7 +28,18 @@ def chatbot(request):
     return render(request, 'chatbot.html') #to show which file to render in the frontend
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatbot')
+        else:
+            error_message='Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
 
 def register(request):
     if request.method=='POST':
@@ -53,3 +64,4 @@ def register(request):
 
 def logout(request):
     auth.logout(request)
+    return redirect('login')
